cb4f0b7fd6e1eb43cc20957be55336f14f64a87f
jcleetus <97258556+jcleetus@users.noreply.github.com>
2022-12-03 20:28:43 -0500

    Tests with mock data

--- Demo App/FeedViewController.swift
+++ Demo App/FeedViewController.swift
@@ -11,6 +11,16 @@ class FeedViewController: UIViewController {
         
     @IBOutlet weak var feedTableView: UITableView!
     
+    // Array of Post objects to simulate real data coming in
+    // Make sure each property that we're assigning to a UI element has a value for each mock Post.
+    var mockData: [Post] = {
+       var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
+       var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
+       var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
+
+       return [meTube, boogle, meTunes]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         feedTableView.delegate = self
@@ -21,14 +31,27 @@ class FeedViewController: UIViewController {
 
 extension FeedViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return mockData.count
     }
     
+    // Creates and configures each cell.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        // Grab an available cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as? PostTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        // Grab a post from our "data"
+        let post = mockData[indexPath.row]
+        // Assign a post to that cell
+        cell.post = post
+        
+        return cell
     }
 }
 
 extension FeedViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
+    }
 }
